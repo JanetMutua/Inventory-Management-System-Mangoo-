a178f7f9d7c022ec290f90f8539be5908b1111f1
JanetMutua <janetmutua04@gmail.com>
2021-12-02 21:15:15 +0300

    created supplier model

--- suppliers/models.py
+++ suppliers/models.py
@@ -3,4 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-# class Supplier(models.Model):
+class Supplier(models.Model):
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    display_name = models.CharField(max_length=100)
+    supplier_email = models.EmailField
+    supplier_phone = models.IntegerField
+    website = models.CharField(max_length=100)
+    address = models.TextField
