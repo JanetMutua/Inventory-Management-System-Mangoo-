21026df65d0d349622eebc017aeab20d5bc40200
JanetMutua <janetmutua04@gmail.com>
2021-12-07 21:32:34 +0300

    started ItemGroups app

--- ItemGroups/models.py
+++ ItemGroups/models.py
@@ -1,3 +1,15 @@
+from os import TMP_MAX
 from django.db import models
 
 # Create your models here.
+
+
+class ItemGroups(models.Model):
+    type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.TextField
+    unit = models.CharField(max_length=100)
+    manufacturer = models.TextField
+    brand = models.TextField
+    attributes = models.TextField
+    tax = models.TextField
--- ItemGroups/views.py
+++ ItemGroups/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import request
 
 # Create your views here.
+
+
+def ItemGroups(request):
+    return render(request, 'ItemGroups/index.html')
