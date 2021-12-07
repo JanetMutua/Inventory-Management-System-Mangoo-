7c85047f8f7a8ab8143d65f3d61c579dc5076bc6
JanetMutua <janetmutua04@gmail.com>
2021-12-07 21:33:15 +0300

    updates on urls.py

--- Mangoo/urls.py
+++ Mangoo/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,4 +25,5 @@ urlpatterns = [
     path('login/', include('login.urls')),
     path('register/', include('register.urls')),
     path('orders/', include('orders.urls')),
+    path('items/', include('ItemGroups.urls')),
 ]
