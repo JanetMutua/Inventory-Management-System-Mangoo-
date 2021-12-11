bc97b03b05c8358416d7602a437ee30c3b42c400
JanetMutua <janetmutua04@gmail.com>
2021-12-11 07:57:28 +0300

    removed recursive data

--- register/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import request
-from django.http import HttpResponse
-# Create your views here.
-
-
-def register(request):
-    return render(request, 'register/index.html')
