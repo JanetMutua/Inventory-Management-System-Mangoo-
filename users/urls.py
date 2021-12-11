89dd8e2d289cc8ba10fc4353ee9c66df94cc512a
JanetMutua <janetmutua04@gmail.com>
2021-12-11 07:58:06 +0300

    started app users

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.login, name='login-page')
+]
+
+
+urlpatterns = [
+    path('', views.register, name='register-page')
+]
