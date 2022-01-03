0fcc42a415b3f4db6ce7c536483200a7b459139f
JanetMutua <janetmutua04@gmail.com>
2022-01-03 21:27:23 +0300

    updates

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.http import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 
 
+# class dashboard_view(ListView):
 def dashboard(request):
     return render(request, 'dashboard/index.html')
